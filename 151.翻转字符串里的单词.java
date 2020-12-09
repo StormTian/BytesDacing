8830a3dfc915a6c4f902b9003babc53a1d70a705
stormTian <906382343@qq.com>
2020-12-09 16:29:52 +0800

    优化151的解法：先前后去空白，用队列去存单词，善用api

--- 151.翻转字符串里的单词.java
+++ 151.翻转字符串里的单词.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /*
@@ -10,35 +12,60 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public String reverseWords(String s) {
-        int len = s.length();
-        if(len <= 1){
-            return s;
+        int left = 0,right = s.length() - 1;
+        //先处理前后的空白字符串
+        while(left <= right && s.charAt(left) == ' ') {
+            left ++;
         }
-        List<String> strArr = new ArrayList<>();
-        StringBuffer tmpBuffer = new StringBuffer();
-        for(int i = 0; i < len; i ++) {
-            if(s.charAt(i) == ' '){
-                if(tmpBuffer.length() > 0){
-                    strArr.add(tmpBuffer.toString());
-                    tmpBuffer = new StringBuffer();
-                }
-                continue;
-            }else {
-                tmpBuffer.append(s.charAt(i));
-            }
-            if(i == len - 1 && tmpBuffer.length() > 0) {
-                strArr.add(tmpBuffer.toString());
-            }
+        while(left <= right && s.charAt(right) == ' ') {
+            right --;
         }
-        StringBuffer ans = new StringBuffer();
-        for(int j = strArr.size() - 1; j >= 0; j --){
-            ans.append(strArr.get(j));
-            if(j != 0){
-                ans.append(' ');
+        Deque<String> d = new ArrayDeque<>();
+        StringBuilder word = new StringBuilder();
+        while(left <= right) {
+            char c = s.charAt(left);
+            if (word.length() > 0 && c == ' ') {
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            }else if (c != ' ') {
+                word.append(c);
             }
+            left ++;
         }
-        return ans.toString();
+        d.offerFirst(word.toString());
+        return String.join(" ", d);
     }
+    // 效率较低
+    // public String reverseWords(String s) {
+    //     int len = s.length();
+    //     if(len <= 1){
+    //         return s;
+    //     }
+    //     List<String> strArr = new ArrayList<>();
+    //     StringBuffer tmpBuffer = new StringBuffer();
+    //     for(int i = 0; i < len; i ++) {
+    //         if(s.charAt(i) == ' '){
+    //             if(tmpBuffer.length() > 0){
+    //                 strArr.add(tmpBuffer.toString());
+    //                 tmpBuffer = new StringBuffer();
+    //             }
+    //             continue;
+    //         }else {
+    //             tmpBuffer.append(s.charAt(i));
+    //         }
+    //         if(i == len - 1 && tmpBuffer.length() > 0) {
+    //             strArr.add(tmpBuffer.toString());
+    //         }
+    //     }
+    //     StringBuffer ans = new StringBuffer();
+    //     for(int j = strArr.size() - 1; j >= 0; j --){
+    //         ans.append(strArr.get(j));
+    //         if(j != 0){
+    //             ans.append(' ');
+    //         }
+    //     }
+    //     return ans.toString();
+    // }
 }
 // @lc code=end
 
